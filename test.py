325d10d898eeae6b30f4b0fad421ff930a8d4d9e
Lesha <qu4rtz43@gmail.com>
2024-03-13 19:36:58 +0300

    fix x coords in test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import requests
 from math import log
 
 
-def search(coords="Москва"):
+def search(coords="город Мадурай"):
     toponym_coodrinates = None
 
     search_address = coords
@@ -28,10 +28,11 @@ def search(coords="Москва"):
 
 def img(coords, z):
     x, y = coords.split(" ")
-    map_request = (f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&"
+    x1, y1 = 18.279018, 0.3
+    x2, y2 = 37.85515, 49.992119
+    map_request = (f"http://static-maps.yandex.ru/1.x/?pt={x},{y},pm2lbm~{x1},{y1},pm2lbm~{x2},{y2},pm2lbm&ll={x},{y}&"
                    f"z={z}&l=map")
     response = requests.get(map_request)
-
     if not response:
         print("Ошибка выполнения запроса:")
         print(map_request)
@@ -45,15 +46,19 @@ def img(coords, z):
 
 
 while True:
-    z = input()
+    z = int(input())
     coodrinates = search()
     img(coodrinates, z)
-
+    kx = 172 / 0.2374 * 2 ** (z - 10)
+    ky = abs(199 / (0.024029 - 0.3) * 2 ** (z - 10))
+    print("K:", ky)
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
 
     clock = pygame.time.Clock()
     running = True
+    x, y = map(float, coodrinates.split())
+    c = 1
     while running:
 
         screen.blit(pygame.image.load("map.png"), (0, 0))
@@ -61,17 +66,22 @@ while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print(event.pos)
-                z = int(z)
-                step_x = int(2 ** (z + 8) / 600)
-                step_y = int(2 ** (z + 8) / 450)
-                print(step_x, step_y)
-                print((event.pos[0] - 300) / abs(event.pos[0] - 300) * abs(event.pos[0] - 300))
-                print((600 - event.pos[1] - 300) / abs(600 - event.pos[1] - 300) * abs(450 - event.pos[1] - 225))
-                y = float(coodrinates.split()[1]) + int((event.pos[0] - 225) / (event.pos[0] - 225)) * log(abs(event.pos[0] - 300) * step_x, 2) - 8
-                x = float(coodrinates.split()[0]) + int((600 - event.pos[1] - 300) / (600 - event.pos[1] - 300)) * log(abs(450 - event.pos[1] - 225) * step_y, 2) - 8
-                print(x,y)
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x2p, y2p = event.pos
+                x2 = (x2p - 300) / kx + x
+                k = abs(ky - abs((0 - y) ** 1.9) * 2 ** (z - 10))     # 36 44
+                print(k)
+                y2 = (450 - y2p - 225) / k + y
+                print(">>", y2)
+                print("pos", event.pos)
+                c += 1
+                x = x2
+                y = y2
+                print("msc", 37.617698, 55.755864)
+                print("cor", 37.617698, 49.992119)
+                print("new", x, y)
+                print("c:", c)
+                print()
                 img(f"{x} {y}", z)
         pygame.display.flip()
     pygame.quit()
